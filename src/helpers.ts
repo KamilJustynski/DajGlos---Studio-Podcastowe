5debb5ccdc6d6c809bfc30cd8eb82144c17c29d2
Kamil Justynski <kamil.justynski1998@gmail.com>
2024-12-05 21:43:56 +0100

    helpers data

---
+++ src/helpers.ts
@@ -0,0 +1,63 @@
+export const NAVBAR_LINKS = [
+  "Dla kogo?",
+  "Co możemy zrobić?",
+  "Zobacz studio",
+  "Wycena",
+];
+
+export const FOR_WHO = [
+  "dla twórców",
+  "dla ludzi biznesu",
+  "dla liderów",
+  "dla specjalistów",
+  "dla przedsiębiorców",
+  "dla Ciebie!",
+];
+
+export const WHAT_WE_DO = [
+  "Podcasty i videocasty",
+  "Content no Social Media (viralowe rolki)",
+  "Kursy Online",
+  "Live i Streamingi",
+  "Landing Page, Sklepy i Strony WWW",
+  "Grafiki, Video i Reklamy",
+];
+
+export const WHAT_WE_DO_SQUARE = [
+  {
+    image: "img1.png",
+    title: "Podcasty i vodeocasty",
+    description:
+      "Przychodzisz i masz do dyspozycji w pełni wyposażone dwa studia. A w nich: światła, kamery, mikrofony meble i rekwizyty, które możemy dopasować do Twojej wizji. Do tego doradzamy na każdym etapie od pomysłu po produkcję i dystrybucję.",
+  },
+  {
+    image: "img2.png",
+    title: "Materiały na SM",
+    description:
+      "Dzięki doświadczeniu i najnowszych narzędzi AI dobieramy najlepszej jakości materiały na Twoje SM, które osiągną najwięcej wyświetleń. Dzięki temu Twoje konta urosną w zawrotnym tempie.",
+  },
+  {
+    image: "img3.png",
+    title: "Kursy video i filmy szkoleniowy",
+    description:
+      "Nagrywamy i montujemy kursy videoi filmy szkoleniowe i filmy instruktażowe.  A dzięki własnemu studiu znacznie obniżamy koszta produkcji. Pomożemy Ci również w stworzeniu landing page i reklam, które sprzedają!",
+  },
+  {
+    image: "img4.png",
+    title: "Landing Page i strony WWW",
+    description:
+      "Każdy biznes, kurs lub marka osobista potrzebuje strony, która sprzedaje. Pomożemy Ci taką stworzyć!. Możemy dla Ciebie także stworzyć  Zarezerwuj konsultację aby omówić szczegóły.",
+  },
+  {
+    image: "img5.png",
+    title: "Webinary i live",
+    description:
+      "Planujesz stworzyć webinar dla swojej społeczności lub klientów? Chcesz zaprezentować nowy produkt lub zareklamować kurs? U nas możesz spokojnie się rozsiąść zajmiemy się organizacją takiej transmisji dla Ciebie.",
+  },
+  {
+    image: "img6.png",
+    title: "Grafiki na SM i reklamy",
+    description:
+      "Możemy stworzyć grafiki i reklamy które dodatkowo zareklamują Twój podcast lub kurs.",
+  },
+];
